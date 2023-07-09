fbe422d701e328748cb8a4629f7a54cbbb452138
milad <miladarabali1@gmail.com>
2023-07-09 21:55:43 +0330

    create Coursestep1 error

---
+++ src/app/Course/create-course/createstep2/createstep2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createstep2',
+  templateUrl: './createstep2.component.html',
+  styleUrls: ['./createstep2.component.css']
+})
+export class Createstep2Component {
+
+}
